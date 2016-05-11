93853b1d4f28f74ffe9cde50fcf2d4b40e4db177
2997215859 <2997215859@qq.com>
2016-05-11 23:51:56 +0800

    1

---
+++ js/app.js
@@ -0,0 +1,51 @@
+var routerApp = angular.module('routerApp',[
+	'ui.router',
+	'ngGrid',
+	'BookListModule',
+	'BookDetailModule']);
+/**
+ * run在程序启动时，只会运行一次
+ * 该步操作，是注入$state和$stateParams服务到root上
+ * 因为整个应用都会使用到路由
+ */
+routerApp.run(function($rootScope,$state,$stateParams){
+	$rootScope.$state = $state;
+	$rootScope.$stateParams = $state;
+});
+
+
+routerApp.config(function($stateProvider,$urlRouterProvider){
+	$urlRouterProvider.otherwise('/index');
+	$stateProvider
+		.state('index',{
+			url:'/index',
+			views:{
+				'':{
+					templateUrl:'tpls/home.html'
+				},
+				'main@index':{
+					templateUrl:'tpls/loginForm.html'
+				}
+			}
+		})
+		.state('booklist',{
+			url:'/{bookType:[0-9]{1,4}}',
+			views:{
+				'':{},
+				'booktype@booklist':{
+					templateUrl:'tpls/bookType.html'
+				},
+				'bookgrid@booklist':{
+					templateUrl:'tpls/bookGrid.html'
+				}
+			}
+		})
+		.state('addbook',{
+			url:'/addbook',
+			templateUrl:'tpls/addBookForm.html'
+		})
+		.state('bookdetail',{
+			url:'/bookdetail/:bookId',
+			templateUrl:'tpls/bookDetail.html'
+		});
+});
